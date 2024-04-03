7f86acdc76e6bfac0dc2033fb32a3a7e8a60bbd1
marinodilauro <m.dilauro90@gmail.com>
2024-04-03 12:45:49 +0200

    Add bootstrap.Add search results on screen

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,5 @@
 <script>
 import { state } from '../state.js';
-import { computed } from 'vue';
 
 export default {
   name: 'AppMain',
@@ -16,8 +15,27 @@ export default {
 
 <template>
 
-  <input type="text" name="searchMovie" id="searchMovie" v-model="searchMovieQuery">
-  <button type="button" @click="this.state.searchMovie(this.searchMovieQuery)">Search movie</button>
+  <div class="search_bar">
+
+    <input type="text" name="searchMovie" id="searchMovie" v-model="searchMovieQuery">
+    <button type="button" @click="this.state.searchMovie(this.searchMovieQuery)">Search movie</button>
+
+  </div>
+
+  <div class="container">
+    <div class="row row-cols-6 gy-1">
+      <div class="col" v-for="movie in this.state.movies">
+
+        <div class="movie_card">
+          <h3 id="movie_title"> {{ movie.title }} </h3>
+          <h6 id="original_title"> {{ movie.originalTitle }} </h6>
+          <p id="language"> {{ movie.lang }} </p>
+          <p id="vote"> {{ movie.vote }} </p>
+        </div>
+      </div>
+    </div>
+
+  </div>
 
 </template>
 
